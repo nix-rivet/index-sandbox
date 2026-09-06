acab28adf4bbccef4d9365105f8b6bd7f7df2336
Jeff Wilde <jeff@complicatedrobot.com>
2026-09-05 21:56:55 -0400

    Publish Nix Rivet 09cf8309d68d5ca2f7285954348ee46a48a9b46737e520e0d2ea11d4572caa80

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.rivet-index = {
-    url = "tarball+https://pub-661f1abeaa7647bc83e673087e6ff162.r2.dev/v0/indexes/sha256/8d16623beea8d0e3e911b3bc14efcc86b17110a775f625b0bdf958c183757a01/index.tar.gz";
+    url = "tarball+https://pub-661f1abeaa7647bc83e673087e6ff162.r2.dev/v0/indexes/sha256/51735d3da5c6f60b7ec5be7633c02a7858d296268651144a073a210438385205/index.tar.gz";
     flake = false;
   };
 
